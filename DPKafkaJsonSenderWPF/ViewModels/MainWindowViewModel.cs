5bcc0ac6b1c1185e83b55df3036fb39e394ea02d
Uchidero Krauch <37419270+asceso@users.noreply.github.com>
2021-09-21 11:48:00 +0300

    release without unity

---
+++ DPKafkaJsonSenderWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using DPKafkaJsonSenderWPF.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace DPKafkaJsonSenderWPF.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private List<AddressModel> TestAddressses;
+
+        private string title = "Формирование JSON для ГУ kafka";
+        private ClientModel client;
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+        public ClientModel Client { get => client; set => SetProperty(ref client, value); }
+
+        public void RaiseClientModel() => RaisePropertyChanged(nameof(Client));
+
+        public DelegateCommand GetRandomAddressModelCommand { get; set; }
+        public DelegateCommand CopyRegistrationAddressToHomeAddressCommand { get; set; }
+        public DelegateCommand CopyLinkResponceCommand { get; set; }
+        public DelegateCommand CopyPassportResponceCommand { get; set; }
+        public DelegateCommand CopyCommonResponceCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Client = new ClientModel();
+            GetRandomAddressModelCommand = new DelegateCommand(OnGetRandomAddress);
+            CopyRegistrationAddressToHomeAddressCommand = new DelegateCommand(OnCopyRegistrationAddressToHomeAddress);
+            CopyLinkResponceCommand = new DelegateCommand(OnCopyLinkResponce);
+            CopyPassportResponceCommand = new DelegateCommand(OnCopyPassportResponce);
+            CopyCommonResponceCommand = new DelegateCommand(OnCopyCommonResponce);
+            FillTestAddresses();
+        }
+        private void FillTestAddresses()
+        {
+            TestAddressses = new List<AddressModel>();
+            TestAddressses.Add(new AddressModel()
+            {
+                AddressStr = "Респ. Алтай, г. Горно-Алтайск, ул. Абаканская, д. 1",
+
+            });
+        }
+
+        private void OnGetRandomAddress()
+        {
+
+        }
+        private void OnCopyRegistrationAddressToHomeAddress()
+        {
+            Client.HomeAddress = (AddressModel)Client.RegistrationAddress.Clone();
+        }
+        private void OnCopyLinkResponce() => Clipboard.SetText(Client.LinkResponce);
+        private void OnCopyPassportResponce() => Clipboard.SetText(Client.PassportResponce);
+        private void OnCopyCommonResponce() => Clipboard.SetText(Client.CommonResponce);
+    }
+}
---
+++ DPKafkaJsonSenderWPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using DPKafkaJsonSenderWPF.ViewModels;
+using System.Windows;
+
+namespace DPKafkaJsonSenderWPF.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly string DTimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+            CalculateTime();
+        }
+        private void CalculateTimeClick(object sender, RoutedEventArgs e)
+        {
+            CalculateTime();
+        }
+        private void CalculateTime()
+        {
+            TimeStampControl.SelectedTime = System.DateTime.Now.AddMinutes(2);
+            RequestStampControl.SelectedTime = System.DateTime.Now.AddMinutes(1);
+            ResponceStampControl.SelectedTime = System.DateTime.Now.AddMinutes(1).AddSeconds(1);
+
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.Client.Timestamp = TimeStampControl.SelectedTime.ToString(DTimeFormat);
+                vm.Client.DTimeRequestDp = RequestStampControl.SelectedTime.ToString(DTimeFormat);
+                vm.Client.DTimeResponseDp = ResponceStampControl.SelectedTime.ToString(DTimeFormat);
+                vm.RaiseClientModel();
+            }
+        }
+        private void TimeStampTimeChanged(object sender, HandyControl.Data.FunctionEventArgs<System.DateTime> e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                if (sender is HandyControl.Controls.TimeBar tb)
+                {
+                    string time_string = e.Info.ToString(DTimeFormat);
+                    if (tb.Name == "TimeStampControl")
+                    {
+                        vm.Client.Timestamp = time_string;
+                    }
+                    else if (tb.Name == "RequestStampControl")
+                    {
+                        vm.Client.DTimeRequestDp = time_string;
+                    }
+                    else if (tb.Name == "ResponceStampControl")
+                    {
+                        vm.Client.DTimeResponseDp = time_string;
+                    }
+                }
+                vm.RaiseClientModel();
+            }
+        }
+    }
+}
